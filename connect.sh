5fca16b917e29f36f3751e3df6d43c346b81da13
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-01-13 12:33:08 -0500

    improved connection helper script
    the script connect.sh can be used to connect to field nodes via the
    manager node as an SSH relay as long as a reverse tunnel exists between
    the field node and the manager node. The local port on the manager node
    listening to the reverse tunnel must be published as part of the DNS
    TXT record for the subdomain.

--- connect.sh
+++ connect.sh
@@ -2,9 +2,33 @@
 
 set -e -o pipefail
 
-DOMAIN="wmc07-f52.sabeti-aws.net"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $(basename $0) subdomain.domain.tld"
+    exit 1
+fi
+
+DOMAIN=$1
+RELAY_DOMAIN="manager.sabeti-aws.net"
+
+# get the IP of the manager node
+# use the AWS nameserver to ensure most current DNS records
+MANAGER_IP=$(dig A +short $RELAY_DOMAIN @ns-491.awsdns-61.com)
 
 # get the port from the DNS TXT record for this subdomain
+# use the AWS nameserver to ensure most current DNS records
 PORT=$(dig TXT +short $DOMAIN @ns-491.awsdns-61.com | awk -F'"' '{ print $2}' | perl -lape 's/^P([0-9]+)\\.*/$1/g')
 
-ssh -o ProxyCommand='ssh -W %h:%p manager' $DOMAIN
+echo "Connecting to '$DOMAIN' via local port '$PORT' on relay '$RELAY_DOMAIN'"
+echo ""
+echo "Note: a live reverse tunnel must be present between '$DOMAIN' and '$RELAY_DOMAIN'"
+echo "      binding the SSH port of '$DOMAIN' to the local port '$PORT' on '$RELAY_DOMAIN.'"
+echo ""
+
+if [[ -z "$PORT" || "$PORT" == " " ]]; then
+        echo "The TXT record for the subdomain specified does begin have a ^PNNN; ... entry"
+        exit 1
+fi
+
+echo "ssh -o ProxyCommand=\"ssh -W %h:%p $MANAGER_IP\" localhost -p $PORT"
+
+ssh -o ProxyCommand="ssh -W %h:%p $MANAGER_IP" localhost -p "$PORT"
