149aef543a382770d77a9ab1ea11510401a45d6b
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-01-14 18:10:13 -0500

    hostname default is now an option in field node setup script

--- setup_field_node_local.sh
+++ setup_field_node_local.sh
@@ -6,11 +6,15 @@ apt-get install -y ansible
 
 echo ""
 echo "========================================================"
-read -p "Enter would you like the hostname of this machine to be: " NODEHOSTNAME
+read -p "Enter would you like the hostname of this machine to be ($(hostname)): " NODEHOSTNAME
 
-echo "127.0.0.1    localhost.localdomain localhost" > /etc/hosts
-echo "127.0.1.1    $NODEHOSTNAME" >> /etc/hosts
-echo "" >> /etc/hosts
-hostnamectl set-hostname $NODEHOSTNAME
+if [[ -z $NODEHOSTNAME ]]; then
+    echo "keeping the same hostname: $(hostname)"
+else
+    echo "127.0.0.1    localhost.localdomain localhost" > /etc/hosts
+    echo "127.0.1.1    $NODEHOSTNAME" >> /etc/hosts
+    echo "" >> /etc/hosts
+    hostnamectl set-hostname $NODEHOSTNAME
+fi
 
 ansible-playbook ./field-node/node-base.yml -i ./production --connection=local --sudo -vvvv
