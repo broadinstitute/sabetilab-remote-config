ebde2f6ee241a207d9968f008179823bc561dcc6
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-02-10 16:46:17 -0500

    additional sensu checks and metrics
    checks for samba and netbios daemons, metrics for uptime and
    temperatures. beginning of adding support for graphite (needs larger
    than nano-sized EC2 intance?)

---
+++ files/install-sensu-gems.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gem install sensu-plugin
+gem install sensu-plugins-network-checks
+gem install sensu-plugins-uptime-checks
+gem install sensu-plugins-environmental-checks
--- files/sensu/plugins/check-for-process.rb
+++ files/sensu/plugins/check-for-process.rb
@@ -3,17 +3,23 @@
 # get the current list of processes
 processes = `ps -A`
 
+if not ARGV[0]
+    p_name = "ps"
+else
+    p_name = ARGV[0]
+end
+
 # determine if the process is running
 running = processes.lines.detect do |process|
-  process.include?('autossh')
+  process.include?(p_name)
 end
 
 # return appropriate check output and exit status code
 if running
-  puts 'OK - autossh process is running'
+  puts 'OK - ' + p_name + ' process is running'
   exit 0
 else
-  puts 'WARNING - autossh process is NOT running'
+  puts 'WARNING - ' + p_name + ' process is NOT running'
   exit 1
 end
 
