1fb7d210e4cc956d06248bc5107749e9f8e399ed
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-02-22 22:19:21 -0500

    added data archival script and cron job
    directories from the samba user directory are moved to an
    ExFAT-formatted USB drive labeled “SEQDATA” if the data directories
    (Illumina run folders) have age >7 days. This commit adds a script to
    find and move the files, and an hourly cron job to execute the script

---
+++ field-node/files/move_old_subdirs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Args are as follows:
+
+# $1 source directory (directory to look within)
+# $2 destination directory (directory to move old directories to)
+# $3 older-than time (15m, 17d, etc.)
+
+if [ "$#" -ne 3 ] || ! [ -d "$1" ] || ! [ -d "$2" ]; then
+  echo "Usage: $0 dir_to_search/ dir_to_move_to/ age" >&2
+  echo ""
+  echo "Where:" 
+  echo "        dir_to_search is the source directory (directory to look within)"
+  echo "        dir_to_move_to is the destination directory (directory to move old directories to)"
+  echo "        age is the older-than time (in days); directories older than this will be moved"
+  exit 1
+fi
+
+SOURCE_DIR="$1"
+DEST_DIR="$2"
+MOVE_OLDER_THAN_DAYS="$3"
+
+if [ -d "$DEST_DIR" ]; then
+    # On OSX we can use -Btime to use the inode birth time, but on Linux mtime is the best we can check
+    find $SOURCE_DIR -maxdepth 1 -type d -mtime +$MOVE_OLDER_THAN_DAYS -exec cp -R --no-preserve=mode,ownership "{}" $DEST_DIR \; -exec rm -r "{}" \;
+fi
+
